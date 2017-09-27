92eef54399b1b21718d1b6665e71aaa47f2f91dc
moh-magdy <moh.mgd@gmail.com>
2017-09-27 03:08:31 +0200

    coler box

--- js/plugins.js
+++ js/plugins.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
   $('.carousel').carousel({
     interval:5000
   });
+
+  // show color option on click
+  $(".gear-check").click(function()
+  {
+    $(".color-option").fadeToggle();
+  });
+
 });
