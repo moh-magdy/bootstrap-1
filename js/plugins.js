431e1b07e613a2aff66ad1559b2db1506e101b78
moh-magdy <moh.mgd@gmail.com>
2017-09-27 21:12:20 +0200

    coloer box finsh

--- js/plugins.js
+++ js/plugins.js
@@ -9,4 +9,17 @@ $(document).ready(function(){
     $(".color-option").fadeToggle();
   });
 
+  // change theme color on click
+  var colorLi = $(".color-option ul li");
+  colorLi
+    .eq(0).css("background-color","#e41b17").end()
+    .eq(1).css("background-color","#337ab7").end()
+    .eq(2).css("background-color","#5cb85c").end()
+    .eq(3).css("background-color","#5bc0de").end()
+    .eq(4).css("background-color","#ba56bb");
+
+  colorLi.click(function(){
+    $("link[href*='theme']").attr("href",$(this).attr("data-value"));
+    //console.log($(this).attr("data-value"));
+  });
 });
